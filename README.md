e70db3ff1b3c64b843fa0fcdd2ac08216d196c9f
Daniel Barcelona-Pons <daniel.barcelona@urv.cat>
2024-04-10 17:36:44 +0200

    Style fixes

---
+++ README.md
@@ -0,0 +1,9 @@
+# Personal academic website
+
+Template credits to https://github.com/sbryngelson/academic-website-template
+
+
+## Build
+
+Run `$ bundle exec jekyll serve --port 9000` in the repository root directory to build and run the site locally.
+
--- update_bootstrap.sh
+++ update_bootstrap.sh
@@ -3,7 +3,7 @@
 # A useful script to download the latest version of bootstrap and jquery
 
 rm -rf node_modules package-lock.json
-npm install bootstrap@4 jquery@3 @popperjs/core
+npm install bootstrap@4 jquery@3 #@popperjs/core
 
 rm -rf _sass/bootstrap
 mkdir -p _sass/bootstrap
@@ -18,8 +18,8 @@ touch assets/javascript/bootstrap/__DO_NOT_MODIFY
 
 # popper stuff
 rm -rf assets/javascript/popper
-mkdir -p assets/javascript/popper
-cp -r node_modules/@popperjs/core/* assets/javascript/popper/
-touch assets/javascript/popper/__DO_NOT_MODIFY
+# mkdir -p assets/javascript/popper
+# cp -r node_modules/@popperjs/core/* assets/javascript/popper/
+# touch assets/javascript/popper/__DO_NOT_MODIFY
 
 rm -rf node_modules package-lock.json
